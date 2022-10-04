dfcea4c856fe8257dc53c2f5a59c313a00d48662
Zeek <mzulkiful@yahoo.com>
2022-10-04 21:59:44 +0100

    Navbar Component Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     
+      <div className='container'>
+        <NavBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+// styles 
+import './Navbar.css'
+
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="navbar">
+      <ul>
+        <li className="logo">
+          <span>Sneakers</span>
+        </li>
+        <li>Collection</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>About</li>
+        
+      </ul>
+    </nav>
+    </div>
+  )
+}
