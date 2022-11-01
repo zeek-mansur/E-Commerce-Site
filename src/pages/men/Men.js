7a6d8e000af5a82e7a63ee68b23efe45701826b2
Zeek <mzulkiful@yahoo.com>
2022-11-01 20:55:03 +0000

    Men And Women Nav page Updated

--- src/pages/men/Men.js
+++ src/pages/men/Men.js
@@ -1,11 +1,26 @@
 import './Men.css'
 
-import React from 'react'
-
+import React, { useState } from 'react'
 export default function Men() {
+
+  const [items] = useState([
+    { title: 'Hoodie', price: 21.99 },
+    { title: 'Men Sneakers', price: 34.99 },
+    { title: 'Men Tee', price: 12.99 }
+  ])
+
   return (
     <div>
-      Men
+    <h3>Latest Offers</h3>
+    <div className="offers">
+      {items.map(item => (
+        <div key={item.title}>
+          <img src="https://via.placeholder.com/350x200" alt="product" />
+          <h4>{item.title}</h4>
+          <p>£{item.price}</p>
+        </div>
+      ))}
     </div>
+  </div>
   )
 }
